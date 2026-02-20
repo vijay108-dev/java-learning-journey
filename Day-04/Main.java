841fcd5b396ddf6200ddc8af2d520ed70c9343ea
vijay108-dev <vijay108.dev@gmail.com>
2026-02-20 13:04:38 +0530

    Add: Day-04: How numbers are stored in Java

---
+++ Day-04/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        float f = 3.14f;   // float value (f lagana zaruri hai agr f nhi lagaya to double treat hoga)
+        //System.out.println(num);
+        System.out.printf(format:"%.20f%n",f);
+    }
+}
